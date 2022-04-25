d192d1d10c172d98778f026b2f2733f0b75c14e0
Youneesz <94466415+Youneesz@users.noreply.github.com>
2022-04-26 00:59:50 +0100

    Added a parent class for Employe called Personne

--- src/main/java/com/sfeproject/employesystem/model/Employe.java
+++ src/main/java/com/sfeproject/employesystem/model/Employe.java
@@ -9,48 +9,15 @@ import java.sql.Date;
 
 @Entity
 @Table(name = "employe")
-public class Employe implements Serializable {
+public class Employe extends Personne implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Id
-    @Column(name = "CODE_EMP", nullable = false)
-    private Integer codeEmp;
+    public Employe(String sexeEmp, Date dateRecrutement, String prenomEmp, String nomEmp, String telEmp, String emailEmp, String adresseEmp, String adresseTravail, String motPasse, String cinEmp) {
+        super(sexeEmp, dateRecrutement, prenomEmp, nomEmp, telEmp, emailEmp, adresseEmp, adresseTravail, motPasse, cinEmp);
+    }
 
-    @Column(name = "CODE_ROLE", nullable = false)
-    private Integer codeRole;
-
-    @Column(name = "CODE_SOMME")
-    private Integer codeSomme;
-
-    @Column(name = "SEXE_EMP")
-    private String sexeEmp;
-
-    @Column(name = "DATE_RECRUTEMENT")
-    private Date dateRecrutement;
-
-    @Column(name = "PRENOM_EMP")
-    private String prenomEmp;
-
-    @Column(name = "NOM_EMP")
-    private String nomEmp;
-
-    @Column(name = "TEL_EMP")
-    private String telEmp;
-
-    @Column(name = "EMAIL_EMP")
-    private String emailEmp;
-
-    @Column(name = "ADRESSE_EMP")
-    private String adresseEmp;
-
-    @Column(name = "ADRESSE_TRAVAIL")
-    private String adresseTravail;
-
-    @Column(name = "MOT_PASSE")
-    private String motPasse;
-
-    @Column(name = "CIN_EMP")
-    private String cinEmp;
-
+    public Employe() {
+        super();
+    }
 }
--- src/main/java/com/sfeproject/employesystem/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sfeproject.employesystem.repository;
-
-import com.sfeproject.employesystem.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface PersonRepository extends JpaRepository<Person, Integer>, JpaSpecificationExecutor<Person> {
-
-}
